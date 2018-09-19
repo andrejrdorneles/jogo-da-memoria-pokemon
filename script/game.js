291d6e03059a9b45020ff6b48c620102bfde3c63
William Cardozo <willian-cardozo@hotmail.com>
2018-09-19 20:44:26 -0300

    wip

---
+++ script/game.js
@@ -0,0 +1,9 @@
+const pokemons = JSON.parse('../pokemons.json');
+
+function loadCards() {
+    const container = document.getElementById("container")
+
+    pokemons.forEach((pokemon) => {
+
+    })
+}
